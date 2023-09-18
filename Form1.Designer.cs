de398f9239a1c49567839d658ef4346f90fc257e
ADMIN <ADMIN@LAB1-MAY06>
2023-09-18 16:19:36 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace Module2BaiSo3_TrinhHongNgoc
+{
+    partial class bntThoat
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTen = new System.Windows.Forms.TextBox();
+            this.btnHo = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtHo = new System.Windows.Forms.TextBox();
+            this.btnTen = new System.Windows.Forms.Button();
+            this.btnHoTen = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.lblHoTen = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(67, 105);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Tên";
+            // 
+            // txtTen
+            // 
+            this.txtTen.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.txtTen.Location = new System.Drawing.Point(148, 98);
+            this.txtTen.Name = "txtTen";
+            this.txtTen.Size = new System.Drawing.Size(237, 20);
+            this.txtTen.TabIndex = 3;
+            // 
+            // btnHo
+            // 
+            this.btnHo.BackColor = System.Drawing.Color.Blue;
+            this.btnHo.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnHo.Location = new System.Drawing.Point(54, 236);
+            this.btnHo.Name = "btnHo";
+            this.btnHo.Size = new System.Drawing.Size(107, 35);
+            this.btnHo.TabIndex = 4;
+            this.btnHo.Text = "&Họ Lót";
+            this.btnHo.UseVisualStyleBackColor = false;
+            this.btnHo.Click += new System.EventHandler(this.btnHo_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(67, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Họ Lót";
+            // 
+            // txtHo
+            // 
+            this.txtHo.BackColor = System.Drawing.Color.White;
+            this.txtHo.Location = new System.Drawing.Point(148, 50);
+            this.txtHo.Name = "txtHo";
+            this.txtHo.Size = new System.Drawing.Size(237, 20);
+            this.txtHo.TabIndex = 1;
+            // 
+            // btnTen
+            // 
+            this.btnTen.BackColor = System.Drawing.Color.Blue;
+            this.btnTen.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTen.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnTen.Location = new System.Drawing.Point(206, 236);
+            this.btnTen.Name = "btnTen";
+            this.btnTen.Size = new System.Drawing.Size(107, 35);
+            this.btnTen.TabIndex = 5;
+            this.btnTen.Text = "&Tên";
+            this.btnTen.UseVisualStyleBackColor = false;
+            this.btnTen.Click += new System.EventHandler(this.btnTen_Click);
+            // 
+            // btnHoTen
+            // 
+            this.btnHoTen.BackColor = System.Drawing.Color.Blue;
+            this.btnHoTen.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHoTen.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnHoTen.Location = new System.Drawing.Point(361, 236);
+            this.btnHoTen.Name = "btnHoTen";
+            this.btnHoTen.Size = new System.Drawing.Size(107, 35);
+            this.btnHoTen.TabIndex = 6;
+            this.btnHoTen.Text = "Họ và Tên";
+            this.btnHoTen.UseVisualStyleBackColor = false;
+            this.btnHoTen.Click += new System.EventHandler(this.btnHoTen_Click);
+            this.btnHoTen.ControlAdded += new System.Windows.Forms.ControlEventHandler(this.btnHoTen_ControlAdded);
+            this.btnHoTen.Enter += new System.EventHandler(this.btnHoTen_Enter);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Blue;
+            this.button4.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button4.Location = new System.Drawing.Point(168, 299);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(184, 35);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Thoát Chương Trình";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // lblHoTen
+            // 
+            this.lblHoTen.AutoSize = true;
+            this.lblHoTen.BackColor = System.Drawing.Color.Cyan;
+            this.lblHoTen.Location = new System.Drawing.Point(1, 1);
+            this.lblHoTen.Name = "lblHoTen";
+            this.lblHoTen.Size = new System.Drawing.Size(0, 13);
+            this.lblHoTen.TabIndex = 3;
+            // 
+            // bntThoat
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(520, 358);
+            this.Controls.Add(this.lblHoTen);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.btnHoTen);
+            this.Controls.Add(this.btnTen);
+            this.Controls.Add(this.btnHo);
+            this.Controls.Add(this.txtHo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtTen);
+            this.Controls.Add(this.label1);
+            this.Name = "bntThoat";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTen;
+        private System.Windows.Forms.Button btnHo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtHo;
+        private System.Windows.Forms.Button btnTen;
+        private System.Windows.Forms.Button btnHoTen;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label lblHoTen;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Module2BaiSo3_TrinhHongNgoc
+{
+    public partial class bntThoat : Form
+    {
+        public bntThoat()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTen_Click(object sender, EventArgs e)
+        {
+           lblHoTen.Text = txtTen.Text;
+        }
+
+        private void btnHo_Click(object sender, EventArgs e)
+        {
+           lblHoTen.Text = txtHo.Text;
+        }
+
+        private void btnHoTen_Click(object sender, EventArgs e)
+        {
+            lblHoTen.Text = txtHo.Text + " " + txtTen.Text;
+        }
+
+        private void btnHoTen_ControlAdded(object sender, ControlEventArgs e)
+        {
+
+        }
+
+        private void btnHoTen_Enter(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
